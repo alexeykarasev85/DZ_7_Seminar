bfe2892a9156f838f5313f62500f0ba5755c6f3a
Alexey <alexeykarasev85@gmail.com>
2023-07-05 22:44:04 +0300

    ДЗ для 7 семинара

---
+++ task2/Program.cs
@@ -0,0 +1,41 @@
+﻿// 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+Console.Write("Введите количество строк массива: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] randomArray = new int[row, column];
+Console.Write("Введите позицию строки: ");
+int row2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите позицию столбца: ");
+int column2 = Convert.ToInt32(Console.ReadLine());
+void mas(int row, int column)
+{
+    int i, j;
+    Random rand = new Random();
+    for (i = 0; i < row; i++)
+    {
+        Console.WriteLine();
+        for (j = 0; j < column; j++)
+        {
+            randomArray[i, j] = rand.Next(0, 10);
+            Console.Write($"{randomArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+mas(row, column);
+if (row2 < 1 || column2 < 1)
+    Console.WriteLine("Позиции не могут быть отрицательными");
+else if (row2 <= row + 1 && column2 <= column + 1)
+    Console.WriteLine($"Значение элемента равно {randomArray[row2 - 1, column2 - 1]}");
+else Console.WriteLine($"Такого элемента нет в массиве");
---
+++ task3/Program.cs
@@ -0,0 +1,53 @@
+﻿// 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void FillMatrixWithRandom(int[,] matrix)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(0, 10);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]}\t ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void SrArif(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[i, j];
+        }
+        System.Console.Write($"{sum / matrix.GetLength(0):F1}\t"); 
+    } 
+System.Console.Write("<-Среднее арифметическое столбцов"); 
+}
+
+System.Console.WriteLine("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[row, column];
+FillMatrixWithRandom(matrix);
+PrintMatrix(matrix);
+SrArif(matrix);
